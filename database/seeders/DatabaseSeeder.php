d04a03edafee6b746771a72ccb398426a03c7098
Nerijus <krekas@gmail.com>
2024-11-08 10:19:15 +0200

    Model, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\City;
+use App\Models\Country;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,7 +19,18 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@admin.com',
         ]);
+
+        Country::create(['name' => 'United States']);
+        Country::create(['name' => 'United Kingdom']);
+        Country::create(['name' => 'Germany']);
+
+        City::create(['country_id' => 1, 'name' => 'New York']);
+        City::create(['country_id' => 1, 'name' => 'Washington']);
+        City::create(['country_id' => 2, 'name' => 'London']);
+        City::create(['country_id' => 2, 'name' => 'Birmingham']);
+        City::create(['country_id' => 3, 'name' => 'Berlin']);
+        City::create(['country_id' => 3, 'name' => 'Stuttgart']);
     }
 }
