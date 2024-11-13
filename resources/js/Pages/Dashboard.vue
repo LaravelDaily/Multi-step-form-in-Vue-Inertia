019bdae9c9553761bfdce99ba0248c7a43d5542a
Nerijus <krekas@gmail.com>
2024-11-13 11:47:17 +0200

    We don't show price in the form

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -143,10 +143,6 @@ const submit = () => {
 					                   {{ form.errors['step3.children'] }}
 				                   </div>
 			                   </div>
-			                   
-			                   <div class="mt-4" v-show="price">
-				                   Price: <span class="font-bold">{{ price }}</span>
-			                   </div>
 		                   </div>
 		                    
 		                    <button class="mt-4 inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900">
