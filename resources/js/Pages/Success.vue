963efd4a2db50cf76b52bb12bacabe04a58e0ff7
Nerijus <krekas@gmail.com>
2024-11-13 11:26:31 +0200

    Change header to success

--- resources/js/Pages/Success.vue
+++ resources/js/Pages/Success.vue
@@ -11,7 +11,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
             <h2
                 class="text-xl font-semibold leading-tight text-gray-800"
             >
-                Dashboard
+                Success
             </h2>
         </template>
 
